c6cd126a78ab656ff4f3d07cb5a0eb27bcede0b7
Dimitrios <dimeragon@gmail.com>
2024-10-14 15:26:46 +0300

    s

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 4000;
 const DILIMITER = ':';
 
 app.use(bodyParser.json());
