2c7866c5dcc5518453804ee48f26120f3079b123
Dimitrios <dimeragon@gmail.com>
2024-10-20 20:54:34 +0300

    5.0.3

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const {User} = require("./user");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
-
+var logger = fs.createWriteStream("data.txt", {
+    flags: "a", // 'a' means appending (old data will be preserved)
+});
+var usersInDb = [];
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,9 +18,8 @@ app.use(
     })
 );
 
-var logger = fs.createWriteStream("data.txt", {
-    flags: "a", // 'a' means appending (old data will be preserved)
-});
+
+initFromDbUsers();
 
 app.get("/participants", (req, res) => {
     const users = [];
@@ -52,7 +54,11 @@ app.post("/participant", async (req, res) => {
     console.log(p);
 
     if (!p.isOk()) {
-        res.send({ error: "Something went wrong" });
+        res.send({ error: "Something went wrong, check the form" });
+        return;
+    }
+    if (useAlreadyExists(p)) {
+        res.send({ error: "User already exists, sorry :(" });
         return;
     }
     await writeToFile(p);
@@ -62,6 +68,35 @@ app.post("/participant", async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+function useAlreadyExists(p){
+    for(const u of usersInDb){
+        if(u.kaggel === p.kaggel || u.gitlab === p.gitlab){
+            return true;
+        }
+    }
+    return false;
+}
+
+function initFromDbUsers(p){
+    const lineReader = require("readline").createInterface({
+        input: fs.createReadStream("data.txt"),
+    });
+
+    lineReader.on("line", function (line) {
+      let p = undefined;
+      try{
+        p = Object.assign(new User(), JSON.parse(line));
+        usersInDb.push(p);
+      }catch(e){
+        console.log("Error parsinsing data file");
+      }  
+    });
+
+    lineReader.on("close", function () {
+        
+    });
+}
 
 async function writeToFile(p) {
     await addLine(JSON.stringify(p));
